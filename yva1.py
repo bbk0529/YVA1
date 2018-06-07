a4d01a2a849bad925272868b0971173464e4d79c
Park <kr6u0102@festo.net>
2018-06-07 09:25:48 +0900

    minor updated for debugging

--- yva1.py
+++ yva1.py
@@ -52,10 +52,8 @@ def itemsParsing(items, so, po) :
     
     TotalList=[]
     SumList=[]
-
     #For each items 
-    start=1
-    
+    start=1    
     #skip the info rows
     if str(items).find("Ship-to party ")>0 :
         start= start +1        
@@ -74,56 +72,40 @@ def itemsParsing(items, so, po) :
             n=n-1
             stored=items[row][-2][76:91].strip()
         
-        status=items[row][0][91:95].strip()
-        #print(items[row])
-        
-        #so=items[row][2][26:36]
-        print(items[row][0][1:5].strip())
+        status=items[row][0][91:95].strip()                   
         no=int(items[row][0][1:5].strip())
         partno=items[row][0][14:22].strip()
         config=items[row][-1][36:80].strip() if items[row][-1][36:80].count("-")>3 else items[1][0][23:62].strip()
-
+        
         #Each records of each Items Parsing
         T1=0;T2=0;T3=0;T4=0
         if str(items[row]).find("Ident")>0 : n-=1
         
-        soidx=items[row][0][1:5]
-        poidx=items[row][1][1:5]
+        soidx=items[row][0].split("|")[1]
+        poidx=items[row][1].split("|")[1]
+        
+        d1=None
         d2=None
         d3=None
         d4=None
         eta=None
+        reference=''
         for i in range(2,n) :
             t=items[row][i]
 
-            if t[11:19].find(".")>0 :
-                d1=t[11:19]
-                #d1=datetime.datetime.strptime(t[11:19],  "%y.%m.%d").date()
-            
-            if t[35:46].find(".")>0 :
-                d2=t[38:46]
-                #d2=datetime.datetime.strptime(t[38:46],  "%y.%m.%d").date() 
-                eta=d2
-
-            if t[85:93].find(".")>0 and t[85:93].find("00.00.00")==-1: 
-                d3=t[85:93]
-                #d3=datetime.datetime.strptime(t[85:93],  "%y.%m.%d").date()
-            #else : 
-                #d3=''
-            
-            if t[116:124].find(".")>0 : 
-                d4=t[116:124]
-                #d4=datetime.datetime.strptime(t[116:124],  "%y.%m.%d").date()
-            #else : 
-                #d4=''
+            if t[11:19].find("/")>0 :d1=t[11:19]              
+            if t[35:46].find("/")>0 : d2=t[38:46]; eta=d2.replace("18/","2018/")
+            if t[85:93].find("/")>0 and t[85:93].find("00/00/00")==-1: d3=t[85:93]                            
+            if t[116:124].find("/")>0 : d4=t[116:124]
+                
 				
             t1=0;t2=0;t3=0;t4=0
-            if t[22:25].strip()!='' : 
-                t1=int(t[22:25])
+            if t[21:25].strip()!='' : 
+                t1=int(t[21:25])
                 T1+=int(t1)    
             
-            if t[49:52].strip()!='' : 
-                t2=int(t[49:52]) 
+            if t[48:52].strip()!='' : 
+                t2=int(t[48:52]) 
                 T2+=int(t2)    
 
             if t[96:99].strip() !="" and t[96:99].find("ED")==-1 : 
@@ -134,23 +116,27 @@ def itemsParsing(items, so, po) :
                 t4=int(t[127:130]);
                 T4+=t4
 
-            #Invoice#     
-            reference=t[-31:-23].strip()
+            #Invoice#
+            if t[-31:-23].strip() !='' :
+                reference=t[-31:-23].strip()
+            
+            print(soidx, reference)
+            
 
             #A single row created here
             TotalList.append([so, soidx.strip(), po, poidx.strip(), (so+ str(no)).strip(), d1,t1,d2,t2,d3,t3,d4,t4,reference])
 
         delta=T1-T4
-        reference=""
+        
         if delta==0:
             etd=TotalList[-1][-3]
-            REMARK="Invoiced"
-            reference=items[row][-2][-31:-23]
+            REMARK="Invoiced"            
+            
         else :
             REMARK=STATUS[status]
             etd=None
 
-        partial="Partial" if delta>0 and T1>delta else ''
+        partial="Partial" if delta>0 and T1>delta else '' #Partial Delivery Case, to be saved in the tab "Partial"
         SumList.append([so, soidx.strip(), po, poidx.strip(), (so+ str(no)).strip(), partno, config, T1,T2,T3,T4, delta, stored, etd, eta, status,REMARK + " " + reference, reference, partial])
 
     return TotalList, SumList 
@@ -161,9 +147,7 @@ def detailDF (conDF, list) :
 
     for i in list:
         print ("DETAIL DF", i)
-        DF=pd.concat([DF, pd.DataFrame(conDF.loc[i], columns=["d1", "t1", "d2", "t2", "d3","t3", "d4", "t4", "reference"])])
-        #print(pd.DataFrame(conDF.loc[str(i)], columns=["d1", "t1", "d2", "t2", "d3","t3", "d4", "t4", "reference"]))
-		#partialDF=pd.concat([partialDF, pd.DataFrame(conDF.loc[i])])
+        DF=pd.concat([DF, pd.DataFrame(conDF.loc[i], columns=["d1", "t1", "d2", "t2", "d3","t3", "d4", "t4", "reference"])])    
     return DF
 
 def mergeDF (filelist) :
@@ -175,25 +159,57 @@ def mergeDF (filelist) :
     for file in filelist :
         print (file, "read to be parsed.... ")
         items, so, po =fileRead(file)
-        TotalList, SumList = itemsParsing(items, so, po)
-        #print("============")
-        #print (file, TotalList)
-        #print("============")
+        TotalList, SumList = itemsParsing(items, so, po)        
         conDF=pd.concat([conDF,pd.DataFrame(TotalList, columns=colDF)])
         conSUM=pd.concat([conSUM,pd.DataFrame(SumList, columns=colSUM)])
-
+    
+    #Converted to DataFrame
     conDF=pd.DataFrame(conDF, columns=colDF)                         
     conSUM=pd.DataFrame(conSUM, columns=colSUM)
     conDF=conDF.set_index("idx")
-    conSUM=conSUM.set_index("idx") 
+    conSUM=conSUM.set_index("idx")        
     
-    
-    
-    partialDF = detailDF(conDF, conSUM[conSUM.Partial=='Partial'].index)
-    
+    partialDF = detailDF(conDF, conSUM[conSUM.Partial=='Partial'].index)    
     return conDF, conSUM, partialDF
 
+def ticketRead(filename) :
+    tickets=[]
+    ticket=[]    
     
+    with open(filename,"r", encoding="ISO-8859-1") as file:
+        for i in range(6) :
+            line=file.readline()
+        line=file.readline()
+        so=line.split()[5]
+        po=line.split()[8]
+        while line.find('*****')<0:
+            #print(line.find('*****'))
+            line=file.readline()
+            if (line.count("|")==15):                
+                posNum=line.split("|")[1].strip()                                                            
+            if (line.count("Terminreklamation") or line.count("Top Priority")) :                
+                line=line.replace("Top Priority", "TopPriority")
+                line=line.replace("fr.", "")
+                
+                ticket=[so] + [posNum] +  line.split()
+                
+                if len(ticket)<=7 :  #if not answered, the length of column is 7 or less. 
+                    for i in range(len(ticketHeader) - len(ticket)) :
+                        ticket.append("")
+                tickets.append(ticket)
+        return tickets
+
+def ticketsToDataFrame(filelist) :
+    conTIckets=[]
+    for file in filelist :
+        tickets=ticketRead(file)
+        conTIckets=conTIckets+tickets
+    
+    conTIckets=pd.DataFrame(conTIckets, columns=ticketHeader)
+    return conTIckets
+
+    
+ticketHeader=["so","pos","no","type","issuedDate","IssuedTime","Initiator","responseDate","responseTime"]   
 
 ##########################
 #
@@ -215,42 +231,45 @@ filelist = [f for f in listdir(mypath) if f.find("2018") == 0 and len(f)==10]
 print(filelist)
 
 DF,SUM,PARTIAL = mergeDF(filelist)
+conTickets=ticketsToDataFrame(filelist)
+
 ##################################################
 #MySQL Database 
 ##################################################
-f=open('mysql.key').readline().split()
+# f=open('mysql.key').readline().split()
 
-try :     
-    cnx = mysql.connector.connect(user=f[0], password=f[1],
-                                  host='127.0.0.1',
-                                  database='lo')
-    cursor = cnx.cursor()    
-    query = "delete from yva1"
-    cursor.execute(query)
-    cnx.commit()
+# try :     
+    # cnx = mysql.connector.connect(user=f[0], password=f[1],
+                                  # host='127.0.0.1',
+                                  # database='lo')
+    # cursor = cnx.cursor()    
+    # query = "delete from yva1"
+    # cursor.execute(query)
+    # cnx.commit()
 
-except Exception as e:
-    print(e)
+# except Exception as e:
+    # print(e)
 
-for k,v in SUM.iterrows():
-    value= (str(list(v)).replace("[","").replace("]","").replace("None","Null"))
-    query = ("INSERT INTO yva1 VALUES(" + value + ") ")
-    print(query)
+# for k,v in SUM.iterrows():
+    # value= (str(list(v)).replace("[","").replace("]","").replace("None","Null"))
+    # query = ("INSERT INTO yva1 VALUES(" + value + ") ")
+    # print(query)
     
-    try :
-        cursor.execute(query)
-    except Exception as e:
-        print(e)
+    # try :
+        # cursor.execute(query)
+    # except Exception as e:
+        # print(e)
         
-cnx.commit()
-cnx.close()
+# cnx.commit()
+# cnx.close()
 ##################################################
 #Excel File output
 ##################################################
 
 #To save excel file 
-writer=pd.ExcelWriter(time.strftime("%Y%m%d")+'YVA1.xlsx')
+writer=pd.ExcelWriter(time.strftime("%Y%m%d%H%M%S")+'YVA1.xlsx')
 SUM.to_excel(writer,'SUM')
 DF.to_excel(writer,'DF')
 PARTIAL.to_excel(writer,'PARTIAL')
+conTickets.to_excel(writer,'tickets')
 writer.save()
